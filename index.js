8416cf397ebab464898b2ef959362462712dae3c
BigFancyBen <benadamsdroid@gmail.com>
2021-08-28 02:06:20 -0600

    add icons, add xp, add multiple rows for loot

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-let { registerFont, createCanvas, Image, loadImage } = require('canvas');
-let canvas = createCanvas(350, 180);
-var ctx = canvas.getContext("2d");
+let { registerFont, createCanvas, loadImage } = require('canvas');
 const axios = require('axios');
-const x = 30;
+const fs = require('fs').promises;
 const express = require('express');
-
+const canvasWidth = 350;
+let ctx = "";
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
@@ -15,9 +14,7 @@ app.listen(process.env.PORT || 3000, function(){
 });
 
 app.post('/getImage', async function (req, res) {
-    console.log(req.body.progress);
     const image = await buildImage(req.body.progress);
-    console.log(image);
     res.send(image);
 })
 
@@ -27,26 +24,35 @@ app.get('/', (req, res) => {
 
 
 async function buildImage(data){
-  console.log(data);
+  let numRows = Math.floor(data.loot.length/7);
+  const canvasHeight = 150 + (60*numRows);
+  let canvas = createCanvas(canvasWidth, canvasHeight);
+  ctx = canvas.getContext("2d");
   registerFont(__dirname.concat('/font/runescape.ttf'), { family: 'Runescape' });
   
-  ctx.fillStyle = "#000";
-  ctx.fillRect(0, 0, 350, 180);
-  ctx.font = "12px Runescape";
+  ctx.fillStyle = "#36393f";
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+  ctx.font = "14px Runescape";
   ctx.textAlign = "left";
   ctx.fillStyle = "#ffff00";
   
   const d = new Date();
+  ctx.strokeText(d.toLocaleDateString('en-US'), 30, 40);
   ctx.fillText(d.toLocaleDateString('en-US'), 30, 40);
   ctx.font = "24px Runescape";
 
-  ctx.fillText(`Runtime: ${data.runtime} minutes`, 30, 70);
+  ctx.strokeText(`🕒${data.runtime} minutes`, 25, 70);
+  ctx.fillText(`🕒${data.runtime} minutes`, 25, 70);
 
   ctx.font = "30px Runescape";
+  ctx.strokeText("Loot:", 30, 110);
   ctx.fillText("Loot:", 30, 110);
 
   let images = await getMultiple(data);
-  let painted = await loadImages(images, data);
+  let itemsReady = await loadItems(images, ctx);
+  let skillsOpened = await openSkills(data.xp_earned);
+  let skillsLoaded = await loadSkills(skillsOpened, ctx);
+
   const response = {
     statusCode: 200,
     body: JSON.stringify(canvas.toDataURL()),
@@ -55,49 +61,74 @@ async function buildImage(data){
   return response;
 }
 
-async function loadImages(objs, data) {
-  let images = [];
-  await Promise.all(objs.map((obj,index) =>
-    loadImage(`data:image/png;base64,${obj}`).then((imageObj) => {
-      paintImages(imageObj, index, data);
+async function loadItems(objs, ctx) {
+  await Promise.all(objs.map((obj, index) =>
+    loadImage(`data:image/png;base64,${obj.img}`).then((imageObj) => {
+      paintIcons(ctx, imageObj, index, obj.count);
     })
   ));
-  return images;
 }
 
-function paintImages(image, index, data){
-    const offset = 40 * index;
-    ctx.drawImage(image, x + offset, 120);
-    ctx.font = "12px Runescape";
-    ctx.textAlign = "right";
-    const count = data.loot[index].count;
-    if (count >= 1000000) {
+async function openSkills(objs) {
+  let skills = [];
+  await Promise.all(objs.map((obj) => 
+    getSkill(`${__dirname}/icons/${obj.skill}.png`).then(response => {
+      skills.push({'img': `data:image/png;base64,${response.toString('base64')}`, 'xp': obj.xp});
+    })
+  ));
+  return skills;
+}
+
+const getSkill = async(file)=>{
+  const result = await fs.readFile(file)
+  return result;
+}
+
+async function loadSkills(objs, ctx) {
+  await Promise.all(objs.map((obj, index) =>
+    loadImage(obj.img).then((imageObj) => {
+      paintSkills(ctx, imageObj, index, `${obj.xp} xp`);
+    })
+  ));
+}
+
+function paintSkills(ctx, image, index, xp){
+  const xOffset = canvasWidth - 30;
+  const yOffset = 40 + (index*30);
+  ctx.drawImage(image, xOffset, yOffset-15);
+  ctx.font = "16px Runescape";
+  ctx.textAlign = "right";
+  ctx.fillStyle = "#ffff00";
+
+  ctx.strokeText(xp, xOffset-5, yOffset);
+  ctx.fillText(xp, xOffset-5, yOffset);
+}
+
+
+function paintIcons(ctx, image, index, count){
+  const xOffset = 30 + (40 * (index % 7));
+  const row = Math.floor(index/7);
+  const yOffset = 120 + (row*40);
+  ctx.drawImage(image, xOffset, yOffset);
+  ctx.font = "12px Runescape";
+  ctx.textAlign = "right";
+  if (count >= 1000000) {
     count = `${Math.floor(count / 1000000)}m`;
-    }
-    if (count > 1000) {
+  }
+  if (count > 1000) {
     count = `${Math.trunc((count * 10) / 1000) / 10}k`;
-    }
-    ctx.lineWidth = 3;
-    ctx.strokeText(count, x + offset + 30, 150);
-    ctx.fillText(count, x + offset+ 30, 150)
+  }
+  ctx.lineWidth = 3;
+  ctx.strokeText(count, xOffset + 30, yOffset+30);
+  ctx.fillText(count, xOffset+ 30, yOffset+30)
 }
 
 async function getMultiple(objectsToGet) {
-  console.log(objectsToGet);
   let items = [];
   await Promise.all(objectsToGet.loot.map(obj =>
     axios.get(`https://api.osrsbox.com/items/${obj.id}`).then(response => {
-      items.push(response.data.icon);
+      items.push({'img': response.data.icon, 'count': obj.count});
     })
   ));
   return items;
-}
-
-function sendResponse(){
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(canvas.toDataURL()),
-  };
-
-  return response;
 }
